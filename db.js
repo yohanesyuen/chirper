62b14c63882f6528da181339bf35b3e842b48fcd
Yohanes Yuen <yuen.wei.ping@gmail.com>
2015-02-09 16:52:36 +0800

    Temporal workaround on database issue by using a hardcoded URI for the db.

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
-var mongooseConfig = {url : 'mongodb://localhost/chirper'};
+//var mongooseConfig = {url : 'mongodb://localhost/chirper'};
+var mongooseConfig = {url : 'mongodb://admin:admin@ds041651.mongolab.com:41651/heroku_app33848434'};
 mongoose.connect(mongooseConfig.url);
 
 module.exports = mongoose;
