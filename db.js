381231bcb2d20017f211e588a7fd6ed121972a23
Yohanes Yuen <yuen.wei.ping@gmail.com>
2015-02-12 04:47:42 +0800

    Test for MONGO_URL environment variable, defaults to use localhost.

--- db.js
+++ db.js
@@ -1,7 +1,12 @@
 var mongoose = require('mongoose');
 
 //var mongooseConfig = {url : 'mongodb://localhost/chirper'};
-var mongooseConfig = {url : 'mongodb://admin:admin@ds041651.mongolab.com:41651/heroku_app33848434'};
+var MONGO_URL;
+if (process.env.MONGO_URL)
+  MONGO_URL = process.env.MONGO_URL;
+else
+  MONGO_URL = 'mongodb://localhost/chirper';
+var mongooseConfig = {url : MONGO_URL};
 mongoose.connect(mongooseConfig.url);
 
 module.exports = mongoose;
