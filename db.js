ca013af63093fb20e95b73dba3cc537428b2ae61
Yohanes Yuen <yuen.wei.ping@gmail.com>
2015-02-12 00:06:16 +0800

    Revert "Test for MONGOLAB_URI environment variable. Defaults to localhost in test environment"
    This reverts commit 8c83634d6ca1571184bf583f555a4e36ea7a4c15.

--- db.js
+++ db.js
@@ -1,9 +1,7 @@
 var mongoose = require('mongoose');
 
-var MONGO_URI;
-MONGO_URI = 'mongodb://localhost/chirper' || process.env.MONGOLAB_URI;
-console.log(MONGO_URI);
-var mongooseConfig = {url : MONGO_URI};
+//var mongooseConfig = {url : 'mongodb://localhost/chirper'};
+var mongooseConfig = {url : 'mongodb://admin:admin@ds041651.mongolab.com:41651/heroku_app33848434'};
 mongoose.connect(mongooseConfig.url);
 
 module.exports = mongoose;
