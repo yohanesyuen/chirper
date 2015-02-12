0668b966c5925bf1d19fd21c632def30c06218cc
Yohanes Yuen <yuen.wei.ping@gmail.com>
2015-02-13 07:46:49 +0800

    Simplified testing for MONGOLAB_URI environment variable when intiating mongoose

--- db.js
+++ db.js
@@ -2,10 +2,9 @@ var mongoose = require('mongoose');
 
 //var mongooseConfig = {url : 'mongodb://localhost/chirper'};
 var MONGO_URL;
-if (process.env.MONGOLAB_URI)
-  MONGO_URL = process.env.MONGOLAB_URI;
-else
-  MONGO_URL = 'mongodb://localhost/chirper';
+
+MONGO_URL = process.env.MONGOLAB_URI || 'mongodb://localhost/chirper';
+
 var mongooseConfig = {url : MONGO_URL};
 mongoose.connect(mongooseConfig.url);
 
