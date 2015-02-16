177688841b9037527206a755bb4267ebe18f0ce0
Yohanes Yuen <yuen.wei.ping@gmail.com>
2015-02-17 00:35:47 +0800

    UI rendering and interaction moved to angular controllers.

---
+++ public/js/chirper.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var chirper = angular.module('chirper', []); // Taking Angular Application in Javascript Variable
+
+// Below is the code to allow cross domain request from web server through angular.js
+chirper.config(['$httpProvider', function($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }
+]);
+
+chirper.controller('newChirpCtrl',['$scope', '$http', function($scope, $http){
+  $scope.chirp = function(){
+    var content = {content: $scope.chirpContent};
+    $http.post('/upload', content);
+  };
+}]);
