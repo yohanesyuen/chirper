8df4dc58f245394b9d90909934d53311db4b5d02
Yohanes Yuen <yuen.wei.ping@gmail.com>
2015-02-16 19:26:16 +0800

    Logic for following and unfollowing added to /api route

---
+++ routes/api.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+var _ = require('lodash');
+var User = require('../models/user');
+
+router.get('/follow/:id', function(req, res){
+  User.findOne({_id: req.user.id}, function(err, _self, done){
+    if (_self && !err) {
+      User.findOne({_id: req.params.id}, function(err, _target, done){
+        if (!err && _target){
+          _self.following.remove(_target._id);
+          _self.following.push(_target._id);
+          _target.followers.remove(_self._id);
+          _target.followers.push(_self._id);
+          _self.save();
+          _target.save();
+          res.send('{new_state: following}');
+        }
+        else
+          done(err);
+      });
+    }
+    else
+      done(err);
+  });
+});
+
+router.get('/unfollow/:id', function(req, res){
+  User.findOne({_id: req.user.id}, function(err, _self){
+    if (_self && !err){
+      User.findOne({_id: req.params.id}, function(err, _target){
+        if (_target && !err){
+          _target = _target;
+          _self.following.remove(_target._id);
+          _target.followers.remove(_self._id);
+          _self.save();
+          _target.save();
+          res.send('{new_state: not-following}');
+        }
+        else
+          done(err);
+      });
+    }
+    else
+      done(err);
+  });
+});
+
+module.exports = router;
