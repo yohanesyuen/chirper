bf348f0f923b5f1b51b72197c51c2b6f8d1182b0
Yohanes Yuen <yuen.wei.ping@gmail.com>
2015-02-20 03:46:44 +0800

    Removed test code. ><

--- public/lib/assets/js/newChirpController.js
+++ public/lib/assets/js/newChirpController.js
@@ -1,10 +1,8 @@
 angular.module('chirper', [])
 
 .controller('newChirpCtrl',['$scope', '$http', function($scope, $http){
-  $scope.chirpContent = 'Chirp Something! :D';
   $scope.chirp = function(){
     var content = {content: $scope.chirpContent};
-    console.log(content.content);
     console.log($http.post('/upload', content));
     $scope.chirpContent = '';
   };
