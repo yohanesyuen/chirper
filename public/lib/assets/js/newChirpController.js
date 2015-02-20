f2a453935c110a2ff382d2b60a4b2349798d8358
Yohanes Yuen <yuen.wei.ping@gmail.com>
2015-02-20 13:15:05 +0800

    Check for empty input before sending Perform trimming before sending (This logic should be shifted to the server eventually)

--- public/lib/assets/js/newChirpController.js
+++ public/lib/assets/js/newChirpController.js
@@ -2,8 +2,12 @@ angular.module('chirper', [])
 
 .controller('newChirpCtrl',['$scope', '$http', function($scope, $http){
   $scope.chirp = function(){
-    var content = {content: $scope.chirpContent};
-    console.log($http.post('/upload', content));
-    $scope.chirpContent = '';
+    if($scope.chirpContent){
+      var content = {content: $scope.chirpContent.trim()};
+      console.log($http.post('/upload', content));
+      $scope.chirpContent = '';
+    }
+    else
+      return;
   };
 }]);
